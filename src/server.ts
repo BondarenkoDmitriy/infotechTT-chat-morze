407faea76aeda176b58f64dbd454fb54229a4c76
BondarenkoDmitriy <bondarenko.dima.kiev@gmail.com>
2023-07-25 13:57:13 +0300

    add authorization for users

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,29 @@
 import express from 'express';
 import http from 'http';
 import { Server, Socket } from 'socket.io';
+import { User } from './types/user';
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server); // Створюємо екземпляр Server, передаючи HTTP сервер
 
+const authorizedUsers: { [username: string]: User } = {};
+
 // Обробка WebSocket з'єднань
 io.on('connection', (socket: Socket) => {
   console.log('Новый клиент подключился');
+
+  // Обробка події 'authorize', яка буде викликана, коли клієнт надішле запит на авторизацію
+  socket.on('authorize', (username: string) => {
+    if (!authorizedUsers[username]) {
+      // Якщо ім'я користувача не існує у структурі даних, додаємо його за участю "user"
+      authorizedUsers[username] = { role: 'user' };
+      console.log(`Пользователь "${username}" успешно авторизован.`);
+    }
+
+    // Надсилаємо клієнту підтвердження авторизації та його роль
+    socket.emit('authorized', { username, role: authorizedUsers[username].role });
+  });
 
   // Обробка події 'message', яка буде викликана, коли клієнт надішле повідомлення
   socket.on('message', (data: string) => {
